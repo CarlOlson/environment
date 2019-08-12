94cbb4ddafddb8c04a4a6534c424d353510ce5a4
CarlOlson <CarlOlson@users.noreply.github.com>
2019-08-13 02:21:41 +0900

    improve pre-tangle

--- pre-tangle.el
+++ pre-tangle.el
@@ -2,15 +2,17 @@
 (require 'cl)
 
 (defun my/babel-tangle-body-hook ()
-  (let ((standard-output (current-buffer))
-        (code (buffer-string)))
+  (let* ((standard-output (current-buffer))
+         (code            (buffer-substring-no-properties (point-min) (point-max)))
+         (context         (substring code 0 (min 50 (length code))))
+         (expanded-code   (cl-macroexpand-all (read (format "(progn %s)" code))))
+         (error-format    (format "Error: %%s, BEGIN\n%s\nEND" context)))
     (delete-region (point-min) (point-max))
-    (princ (cl-subst code '%s
-                     `(let ((lexical-binding t))
-                        (condition-case err
-                            (progn %s)
-                          (error
-                           (run-with-idle-timer
-                            0 nil (lambda () (signal (car err) (cdr err)))))))))))
+    (prin1 `(let ((debug-on-error nil))
+              (with-demoted-errors ,error-format
+                (condition-case err ,expanded-code
+                  (error
+                   (run-with-idle-timer 0 0 (lambda () (switch-to-buffer "*Messages*")))
+                   (signal (car err) (cdr err)))))))))
 
 (add-to-list 'org-babel-tangle-body-hook 'my/babel-tangle-body-hook)
