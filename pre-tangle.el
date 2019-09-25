2560e08a90618cf1afd9fd74ea000839009a8adb
CarlOlson <CarlOlson@users.noreply.github.com>
2019-09-26 01:47:27 +0900

    simplify pre-init and key remapping

--- pre-tangle.el
+++ pre-tangle.el
@@ -1,18 +1,17 @@
+;;; -*- lexical-binding: t; -*-
+
 (require 'org)
 (require 'cl)
 
-(defun my/babel-tangle-body-hook ()
-  (let* ((standard-output (current-buffer))
-         (code            (buffer-substring-no-properties (point-min) (point-max)))
-         (context         (substring code 0 (min 50 (length code))))
-         (expanded-code   (cl-macroexpand-all (read (format "(progn %s)" code))))
-         (error-format    (format "Error: %%s, BEGIN\n%s\nEND" context)))
-    (delete-region (point-min) (point-max))
-    (prin1 `(let ((debug-on-error nil))
-              (with-demoted-errors ,error-format
-                (condition-case err ,expanded-code
-                  (error
-                   (run-with-idle-timer 0 0 (lambda () (switch-to-buffer "*Messages*")))
-                   (signal (car err) (cdr err)))))))))
+(let (added-header)
+  (defun my/babel-tangle-body-hook ()
+    (let* ((standard-output (current-buffer))
+           (code            (buffer-substring-no-properties (point-min) (point-max)))
+           (expanded-code   (cl-macroexpand-all (read (format "(progn %s)" code)))))
+      (delete-region (point-min) (point-max))
+      (unless added-header
+        (princ ";;; -*- lexical-binding: t; -*-\n")
+        (setq added-header t))
+      (prin1 expanded-code))))
 
 (add-to-list 'org-babel-tangle-body-hook 'my/babel-tangle-body-hook)
