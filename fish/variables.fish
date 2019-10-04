5518ab807dc3687db177e9e8d3294785066fe782
CarlOlson <CarlOlson@users.noreply.github.com>
2019-10-05 00:07:34 +0900

    flesh out fish config

---
+++ fish/variables.fish
@@ -0,0 +1,7 @@
+set -e ALTERNATE_EDITOR
+set -gx EDITOR emacsclient
+set -gx FZF_CTRL_T_COMMAND fd --type f
+set -gx FZF_DEFAULT_COMMAND fd --type f
+set -gx HEX_HTTP_CONCURRENCY 1
+set -gx HEX_HTTP_TIMEOUT 60
+set -gx SPELL aspell -x -c
