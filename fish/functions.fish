15474773306d1513bade57f40599334011589051
CarlOlson <CarlOlson@users.noreply.github.com>
2019-10-05 13:47:53 +0900

    refine git status in fish_prompt

--- fish/functions.fish
+++ fish/functions.fish
@@ -1,67 +1,48 @@
 
-if not set -q -g __fish_classic_git_functions_defined
-    set -g __fish_classic_git_functions_defined
-
-    function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-        if status --is-interactive
-            commandline -f repaint 2>/dev/null
-        end
-    end
-
-    function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-        if status --is-interactive
-            commandline -f repaint 2>/dev/null
-        end
-    end
-
-    function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-        if status --is-interactive
-            commandline -f repaint 2>/dev/null
-        end
-    end
-
-    function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-        if status --is-interactive
-            commandline -f repaint 2>/dev/null
-        end
-    end
-
-    # initialize our new variables
-    if not set -q __fish_classic_git_prompt_initialized
-        set -qU fish_color_user
-        or set -U fish_color_user -o green
-        set -qU fish_color_host
-        or set -U fish_color_host -o cyan
-        set -qU fish_color_status
-        or set -U fish_color_status red
-        set -U __fish_classic_git_prompt_initialized
-    end
-end
-
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
     set -l normal (set_color normal)
-    set -l color_cwd
-    set -l prefix
-    set -l suffix
-
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix (set_color purple) ' » ' $normal
-    end
-
+    set -l yellow (set_color yellow)
+    set -l suffix (set_color purple) '» ' $normal
     set -l prompt_status
+    set -l git_status
+    set -l git_info
+
     if test $last_status -ne 0
-        set prompt_status ' ' (set_color $fish_color_status) "$last_status"
+        set prompt_status (set_color red) "$last_status "
     end
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal $prompt_status $normal (__fish_vcs_prompt) $suffix
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+        # if command git diff-index --quiet HEAD --
+        if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
+            echo $count | read -l ahead behind
+            if test "$ahead" -gt 0
+                set git_status "$git_status"(set_color red)⬆
+            end
+            if test "$behind" -gt 0
+                set git_status "$git_status"(set_color red)⬇
+            end
+        end
+
+        for i in (git status --porcelain | string sub -l 2 | uniq)
+            if set -l char (string match -r '^[ADMR]' $i)
+                set git_status "$git_status"(set_color green)$char
+            end
+            if set -l char (string match -r '[ADMR]$' $i)
+                set git_status "$git_status"(set_color red)$char
+            end
+            if set -l char (string match -r '[CU]' $i)
+                set git_status "$git_status"(set_color yellow)$char
+            end
+            if test $i = '??'
+                set git_status "$git_status"(set_color red)'?'
+            end
+        end
+        # end
+
+        set git_status (echo $git_status | uniq | string join '')
+        set git_info "$yellow‹$git_branch $git_status$yellow› "
+    end
+
+    echo -n -s (set_color green) "$USER" $normal @ (set_color cyan) (prompt_hostname) $normal ' ' (set_color $fish_color_cwd) (prompt_pwd) ' ' $prompt_status $git_info $suffix
 end
