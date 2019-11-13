6b8459e16e410c1a553de62ccb1e3d69b99f0479
CarlOlson <CarlOlson@users.noreply.github.com>
2019-11-13 11:07:25 +0900

    fix for windows display

--- fish/variables.fish
+++ fish/variables.fish
@@ -9,8 +9,11 @@ set -gx PATH $HOME/git/scripts $PATH
 set -gx TMUX_TMPDIR /tmp
 
 if test -d /mnt/c
-  set -gx PATH $HOME/.nix-profile/bin $PATH
-  set -gx NIX_PATH $HOME/.nix-defexpr/channels
-  set -gx NIX_PROFILES /nix/var/nix/profiles/default $HOME/.nix-profile
-  set -gx NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
+    set -gx PATH $HOME/.nix-profile/bin $PATH
+    set -gx NIX_PATH $HOME/.nix-defexpr/channels
+    set -gx NIX_PROFILES /nix/var/nix/profiles/default $HOME/.nix-profile
+    set -gx NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
+
+    set -l ip (rg -N 'nameserver (.*)' -r '$1' /etc/resolv.conf)
+    set -gx DISPLAY $ip:0.0
 end
