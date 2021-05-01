f410bdff7660482d7aefa49cd09953717a963183
CarlOlson <CarlOlson@users.noreply.github.com>
2021-05-01 22:29:54 +0900

    add typescript config

--- elisp/config/javascript.el
+++ elisp/config/javascript.el
@@ -25,3 +25,15 @@
 
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . jsx-web-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . json-web-mode))
+
+(defun my/typescript-mode-hook ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1)
+  (eldoc-mode +1)
+  (setq-local flycheck-check-syntax-automatically '(save mode-enabled))
+  (add-hook 'before-save-hook 'tide-format-before-save nil t))
+
+(add-hook 'typescript-mode-hook #'my/typescript-mode-hook)
