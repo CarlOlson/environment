17b863d7d7190ae445987cc85745dadd3a95b3fc
CarlOlson <CarlOlson@users.noreply.github.com>
2023-04-29 18:33:14 +0900

    update rust.el

--- elisp/config/rust.el
+++ elisp/config/rust.el
@@ -1,4 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 
-(add-hook 'rust-mode-hook 'company-mode)
-(add-hook 'rust-mode-hook 'racer-mode)
+(defun my/rust-mode-hook ()
+  (interactive)
+  (setq-local company-idle-delay 0.2
+              company-minimum-prefix-length 1
+              lsp-signature-render-documentation nil))
+
+(with-eval-after-load "rust-mode"
+  (define-key rust-mode-map (kbd "<tab>") 'company-indent-or-complete-common)
+  (add-hook 'rust-mode-hook 'my/rust-mode-hook)
+  (add-hook 'rust-mode-hook 'yas-minor-mode-on))
--- elisp/keybindings/rust.el
+++
@@ -1,5 +0,0 @@
-;;; -*- lexical-binding: t; -*-
-
-(with-eval-after-load 'rust-mode
-  (defvar rust-mode-map)
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common))
