b0852face62d4ee623eb99ca4f136d89547246c8
CarlOlson <CarlOlson@users.noreply.github.com>
2023-04-29 18:34:26 +0900

    update javascript.el

--- elisp/config/javascript.el
+++ elisp/config/javascript.el
@@ -1,36 +1,36 @@
 ;;; -*- lexical-binding: t; -*-
 
-(assoc-delete-all 'yarn projectile-project-types)
-(projectile-register-project-type
- 'yarn '("yarn.lock")
- :test "yarn test"
- :run "yarn start"
- :related-files-fn (list
-                    (projectile-related-files-fn-test-with-suffix "re" "_Test")
-                    (projectile-related-files-fn-test-with-suffix "rei" "_Test")
-                    (projectile-related-files-fn-test-with-suffix "js" ".spec")))
-
 (define-derived-mode jsx-web-mode web-mode "jsx-Web"
   "Version of web-mode just for js and jsx files."
   (setq-local web-mode-markup-indent-offset 2)
   (setq-local web-mode-code-indent-offset 2)
   (setq-local web-mode-enable-auto-quoting nil)
-  (web-mode-set-content-type "jsx"))
+  (web-mode-set-content-type "jsx")
+  (prettier-mode +1))
 
 (define-derived-mode json-web-mode web-mode "json-Web"
   "Version of web-mode just for js and jsx files."
   (setq-local web-mode-markup-indent-offset 2)
   (setq-local web-mode-code-indent-offset 2)
   (setq-local web-mode-enable-auto-quoting nil)
-  (web-mode-set-content-type "json"))
+  (web-mode-set-content-type "json")
+  (prettier-mode +1))
 
 (define-derived-mode snap-web-mode jsx-web-mode "snap-Web"
   "Version of web-mode just Jest snap files."
-  (setq-local before-save-hook nil))
+  (setq-local before-save-hook nil)
+  (prettier-mode -1))
 
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . jsx-web-mode))
+(define-derived-mode css-web-mode web-mode "css-Web"
+  "Version of web-mode just for CSS files."
+  (setq-local web-mode-markup-indent-offset 2)
+  (web-mode-set-content-type "css")
+  (prettier-mode +1))
+
+(add-to-list 'auto-mode-alist `(,(rx "." (or "js" "jsx" "cjs" "mjs") eol) . jsx-web-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . json-web-mode))
 (add-to-list 'auto-mode-alist '("\\.snap$" . snap-web-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . css-web-mode))
 
 (defun my/typescript-mode-hook ()
   (interactive)
@@ -39,7 +39,17 @@
   (tide-hl-identifier-mode +1)
   (company-mode +1)
   (eldoc-mode +1)
-  (setq-local flycheck-check-syntax-automatically '(save mode-enabled))
-  (add-hook 'before-save-hook 'tide-format-before-save nil t))
+  (prettier-mode +1)
+  (setq-local flycheck-check-syntax-automatically '(save mode-enabled)))
 
 (add-hook 'typescript-mode-hook #'my/typescript-mode-hook)
+
+(add-hook 'css-mode #'prettier-mode)
+
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration '(jsx-web-mode . "javascript"))
+  (add-to-list 'lsp-language-id-configuration '(snap-web-mode . "javascript"))
+  (add-to-list 'lsp-language-id-configuration '(json-web-mode . "json")))
+
+(with-eval-after-load 'web-mode
+  (define-key web-mode-map (kbd "C-c C-f") nil))
