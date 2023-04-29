a3061d457a2d07af598a797037da8686d40e8e18
CarlOlson <CarlOlson@users.noreply.github.com>
2023-04-29 18:34:51 +0900

    update reason.el

--- elisp/config/reason.el
+++ elisp/config/reason.el
@@ -12,27 +12,19 @@
 (with-eval-after-load 'lsp-mode
   (lsp-register-client
    (make-lsp-client
-    :new-connection (lsp-stdio-connection "/home/carl/.local/rls-linux/reason-language-server")
-    :major-modes '(reason-mode)
-    :notification-handlers (ht ("client/registerCapability" 'ignore))
-    :priority 1
-    :server-id 'reason-ls))
-
-  (lsp-register-client
-   (make-lsp-client
     :new-connection (lsp-stdio-connection "/home/carl/git/environment/rescript-lsp.sh")
     :major-modes '(rescript-mode)
     :priority 1
     :server-id 'rescript-ls))
   (add-to-list 'lsp-language-id-configuration (cons ".*\\.resi?$" "rescript")))
 
-(defun reason-fill-paragraph ()
+(defun rescript-fill-paragraph ()
   (interactive)
   (with-temp-buffer-swap
     (c-mode)
     (c-fill-paragraph)))
 
-(defun my/reason-mode-hook ()
+(defun my/rescript-mode-hook ()
   (lsp t)
   (setq-local company-minimum-prefix-length 1)
   (setq-local company-idle-delay 0.2)
@@ -43,27 +35,32 @@
 
 (cl-defun bsc-format-buffer ()
   (interactive)
-  (when (buffer-modified-p)
-    (save-buffer)
-    (when (member 'bsc-format-buffer after-save-hook)
-      (cl-return-from bsc-format-buffer)))
-  (let ((file-name (buffer-file-name))
-        (this-buffer (current-buffer))
+  ;; (when (buffer-modified-p)
+  ;;   (save-buffer)
+  ;;   (when (member 'bsc-format-buffer after-save-hook)
+  ;;     (cl-return-from bsc-format-buffer)))
+  (let ((this-buffer (current-buffer))
         (extension (file-name-extension (buffer-file-name) t)))
+    (when (buffer-modified-p)
+      (delete-trailing-whitespace))
     (with-temp-buffer
       (anaphoric-block nil
         (replace-buffer-contents this-buffer)
-        (call-process-region (point-min) (point-max) "yarn" t t nil
-                      "run" "rescript" "format" "-stdin" extension)
+        ;; (if (f-join (projectile-project-root) "yarn.lock")
+        ;;     (call-process-region (point-min) (point-max) "yarn" t t nil
+        ;;                          "run" "rescript" "format" "-stdin" extension)
+        (call-process-region (point-min) (point-max) "rescript" t t nil
+                             "format" "-stdin" extension)
+        ;; )
         (when (not (zerop it))
           (message "%s" (buffer-substring-no-properties (point-min) (point-max)))
           (cl-return))
         (let ((temp-buffer (current-buffer)))
           (with-current-buffer this-buffer
-            (replace-buffer-contents temp-buffer)))))))
+            (replace-buffer-contents temp-buffer)
+            (save-buffer)))))))
 
-(add-hook 'reason-mode-hook #'my/reason-mode-hook)
-(add-hook 'rescript-mode-hook #'my/reason-mode-hook)
+(add-hook 'rescript-mode-hook #'my/rescript-mode-hook)
 
 (with-eval-after-load 'reason-mode
   (setq auto-mode-alist (cl-remove-if
@@ -71,3 +68,32 @@
                          auto-mode-alist))
   (add-to-list 'auto-mode-alist '("\\.rei?$" . reason-mode))
   (add-to-list 'auto-mode-alist '("\\.resi?$" . rescript-mode)))
+
+(cl-defun reason-to-rescript ()
+  (interactive)
+  (require 'dash)
+  (when (buffer-modified-p (current-buffer))
+    (message "Save buffer first!")
+    (cl-return-from reason-to-rescript))
+  (let* ((re-buffer (current-buffer))
+         (re-file-name (buffer-file-name))
+         (is-interface (s-ends-with? "i" (buffer-file-name)))
+         (res-ext (if is-interface ".resi" ".res"))
+         (res-file-name
+          (->
+           (buffer-file-name)
+           (file-name-sans-extension)
+           (concat res-ext))
+          )
+         (status-code
+          (call-process "yarn" nil nil nil "dlx" "rescript@9" "convert" re-file-name)))
+    (when (eq status-code 0)
+      (find-file-existing res-file-name)
+      (kill-buffer-if-not-modified re-buffer))))
+
+(defun bsrefmt-file ()
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (point (point)))
+    (call-process-region nil nil "bsrefmt" t t nil)
+    (goto-char point)))
--- elisp/keybindings/reason.el
+++ elisp/keybindings/reason.el
@@ -1,6 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
-(with-eval-after-load 'reason-mode
-  (define-key reason-mode-map (kbd "M-q") 'reason-fill-paragraph))
+(with-eval-after-load 'rescript-mode
+  (define-key rescript-mode-map (kbd "M-q") 'rescript-fill-paragraph)
+  (define-key rescript-mode-map (kbd "C-c p s") 'projectile-toggle-between-implementation-and-test)
+  (define-key rescript-mode-map (kbd "C-c p t") 'projectile-test-project)
+  (define-key rescript-mode-map (kbd "C-c p c") 'projectile-compile-project))
 
-(define-key rescript-mode-map (kbd "C-c <tab>") 'bsc-format-buffer)
+(with-eval-after-load 'reason-mode
+  (define-key reason-mode-map (kbd "C-c <tab>") 'bsrefmt-file))
